43a28c6e6aed10553e041e07b76abb667cf2aca3
Lee Gillentine <lee@fishburd.com>
2013-03-27 16:18:31 -0400

    Checking in initial setup

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 php-for-cool-kids
 =================
 
-PHP boilerplate with Slim, Coffeescript, and Haml
+PHP boilerplate with Slim, Coffeescript, Haml, Scss (NOT Sass)
+
+
+* [Slim](http://www.slimframework.com)
+* Haml support provided by [MtHaml](https://github.com/arnaud-lb/MtHaml)
+* CoffeeScript support provided by [Coffeescript-php](https://github.com/alxlit/coffeescript-php)
+* Scss support provided by [Scssphp](https://github.com/leafo/scssphp)
+
+Installation Instructions
+========================
+
+
+1. Install composer in the project `curl -s https://getcomposer.org/installer | php`
+2. Install everything via composer `php composer.phar install`
+3. Make sure your `.htaccess` or `httpd.conf` is set up correctly (see `.htaccess` in the repo for an example)
+
---
+++ index.php
@@ -0,0 +1,39 @@
+<?php
+require 'vendor/autoload.php';
+
+
+$app = new \Slim\Slim();
+
+$app->get('/', function(){
+  //ok, def need to this out... ideally haml will be a renderer for slim
+  $haml = new MtHaml\Environment('php');
+  $template = __DIR__."/views/index.haml";
+  $compiled = $haml->compileString(file_get_contents($template), $template);
+  echo $compiled;
+
+});
+
+$app->get('/foo.js', function(){
+  $file = __DIR__."/app/js/foo.coffee";
+  try
+  {
+    $coffee = file_get_contents($file);
+    $js = CoffeeScript\Compiler::compile($coffee, array('filename' => $file));
+    echo $js;
+  }
+  catch (Exception $e)
+  {
+    echo $e->getMessage();
+  }
+
+});
+
+$app->get('/foo.css', function(){
+  $scss = new scssc();
+  $file = file_get_contents(__DIR__."/app/css/foo.scss");
+  echo $scss->compile($file);
+});
+
+
+
+$app->run();
